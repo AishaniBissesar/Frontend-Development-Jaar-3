95175dcde7367715ac65c60c512afc9479413286
AishaniBissesar <94417167+AishaniBissesar@users.noreply.github.com>
2022-09-28 14:29:09 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("hi");
+
+var menuButton = document.querySelector("header button");
+
+menuButton.addEventListener("click", menuToggle);
+
+function menuToggle() {
+    var deNav = document.querySelector ("header > nav");
+
+    deNav.classList.toggle("open");
+}
